281df51300576cf024fbfd50dde29f850ef30c1a
Habeeb602 <jhabeeb602@gmail.com>
2021-10-16 14:51:24 +0530

    Habeeb Rahman J 20MCA1025 Completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const success = () => {
+    const txt = `    <h1 style=' display: flex; justify-content: center; align-items: center;'>
+    *** Thank you ***     
+</h1>`
+    document.write(txt);
+}
